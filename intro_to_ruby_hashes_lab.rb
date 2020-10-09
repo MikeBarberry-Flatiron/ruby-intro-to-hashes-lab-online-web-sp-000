89c304748a4519bd903946a64e0066b8a05cdad0
Mike Barberry <mikebarberry@protonmail.com>
2020-10-09 23:43:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ end
 def monopoly
 	monopoly = {
 		railroads: {
-			
+
 		}
 	}
 end
@@ -22,6 +22,9 @@ def monopoly_with_second_tier
 	monopoly = {
 		railroads: {
 			pieces: 4
+		}
+		names: {
+			
 		}
 	}
 end
